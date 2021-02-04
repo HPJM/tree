5576763fbe7d8f9ba83a0565587eaaa4c3caad55
Hugo Morgan <hpjm37@gmail.com>
2021-02-04 14:15:46 +0000

    remove boilerplate, implement add/2 and new/1

--- lib/tree.ex
+++ lib/tree.ex
@@ -1,18 +1,37 @@
 defmodule Tree do
   @moduledoc """
-  Documentation for `Tree`.
+  Represents a generic `Tree`.
   """
 
+  defmodule Node do
+    defstruct [:data, children: []]
+  end
+
   @doc """
-  Hello world.
+  Generates a node.
 
   ## Examples
 
-      iex> Tree.hello()
-      :world
+      iex> Tree.new(2)
+      %Tree.Node{data: 2}
 
   """
-  def hello do
-    :world
+  def new(data) do
+    %Node{data: data}
+  end
+
+  @doc """
+  Adds a node as a child of the tree.
+
+  ## Examples
+
+      iex> tree = Tree.new(2) |> Tree.add(4)
+      iex> %Tree.Node{children: [child]} = tree
+      iex> child.data
+      4
+
+  """
+  def add(%Node{children: children} = tree, data) do
+    %Node{tree | children: [new(data) | children]}
   end
 end
--- test/tree_test.exs
+++ test/tree_test.exs
@@ -1,8 +1,22 @@
 defmodule TreeTest do
   use ExUnit.Case
   doctest Tree
+  alias Tree.Node
 
-  test "greets the world" do
-    assert Tree.hello() == :world
+  describe "new/1" do
+    test "creates a Node" do
+      assert Tree.new(2) == %Node{data: 2, children: []}
+    end
+  end
+
+  describe "add/2" do
+    test "adds a node to tree as child" do
+      root = Tree.new(2)
+
+      assert Tree.add(root, 4) == %Tree.Node{
+               children: [%Tree.Node{children: [], data: 4}],
+               data: 2
+             }
+    end
   end
 end
